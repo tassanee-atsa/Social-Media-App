d86b88f91284535bdc480139e490f531b0cef8e2
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-05 22:48:39 +0000

    fix import syntax

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from bodyParser;
+import bodyParser from "bodyParser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
