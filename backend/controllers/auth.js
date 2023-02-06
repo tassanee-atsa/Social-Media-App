91e4fbb7722cff1083e0d03518248b07ed8f91b7
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-06 22:45:42 +0000

    add bcrypt function

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -8,10 +8,10 @@ import User from "../models/User.js";
 //This has to be async function as we will call Mongoose DB, 
 //req => request body that we get from the frontend,
 // res => response is what we will send back to frontend.
-// Express will provide rew and res by default
+// Express will provide req and res by default
 export const register = async (req, res) => {
     try {
-        //We will deconstruction these functions
+        //We will deconstructing these functions from the req.body 
         const {
             firstName, 
             lastName,
@@ -21,8 +21,10 @@ export const register = async (req, res) => {
             friends,
             location,
             occupation
-        } = req.body 
-
+        } = req.body ;
+//We will create random salt provided by bcrypt
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
     } catch (err) {
 
     }
