c321281b6fb68709fd06925c5b984bbb46ba948f
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-06 22:07:56 +0000

    fixed error value Schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,20 +3,32 @@ import mongoose from 'mongoose'
 const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: ture,
+    required: true,
     min: 2, //min 2 value
     max: 50,
   },
   lastName: {
     type: String,
-    required: ture,
+    required: true,
     min: 2,
     max: 50,
   },
   email: {
     type: String,
-    required: ture,
+    required: true,
     max: 50,
-    unique: true // you can not have duplicate email
-  }
-})
+    unique: true, // you can not have duplicate email
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 5,
+    unique: true, // you can not have duplicate email
+  },
+  email: {
+    type: String,
+    default: " ",
+  
+  },
+
+});
