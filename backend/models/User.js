003886bb2571bdcf3a759c9a6d8096a745a3177b
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-06 00:02:34 +0000

    add email schema and comments

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
     require: ture,
-    min: 2,
+    min: 2, //min 2 value
     max: 50,
   },
   lastName: {
@@ -13,4 +13,10 @@ const UserSchema = new mongoose.Schema({
     min: 2,
     max: 50,
   },
+  email: {
+    type: String,
+    require: ture,
+    max: 50,
+    unique: true // you can not have duplicate email
+  }
 })
