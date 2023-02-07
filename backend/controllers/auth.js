73fb113c6f70a4750ad2835557da7abbd68210ed
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-07 22:49:23 +0000

    added token jwt

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -58,6 +58,8 @@ export const login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. "})
 
+        const token = jwt.sign({ id: user._id } , process.env.JWT_SECRET);
+
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
