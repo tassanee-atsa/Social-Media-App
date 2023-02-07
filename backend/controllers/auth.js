626f302a99c02e3184139f3692f5e4e99c28ef67
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-07 16:32:20 +0000

    add comments savedUser

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -41,8 +41,8 @@ export const register = async (req, res) => {
             viewProfile: Math.floor(Math.random()*10000),
             impressions: Math.floor(Math.random()*10000),
         });
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
+        const savedUser = await newUser.save(); //To save the user
+        res.status(201).json(savedUser); //If above does not error out, we will send the user status 201 with json saveUser
     } catch (err) {
       res.status(500).json({error: err.message}); //response message to the frontend if anything goes wrong
     }
