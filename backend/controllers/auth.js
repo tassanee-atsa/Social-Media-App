57ff5d59d9ad8f9f0359de580ca70f54f4cc28e2
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-07 22:54:17 +0000

    added delete user

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -59,7 +59,8 @@ export const login = async (req, res) => {
         if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. "})
 
         const token = jwt.sign({ id: user._id } , process.env.JWT_SECRET);
-
+        delete user.password;
+        res.status(200).json({ token, user });
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
