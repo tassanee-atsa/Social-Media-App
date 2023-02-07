6dd4b7dcc6c75fb01549493c28fe45a1d6dccc43
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-07 22:47:19 +0000

    validate a user:auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -53,7 +53,11 @@ export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email: email });
-        
+        if (!user) return res.status(400).json({ msg: "User does not exist. "});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. "})
+
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
