b7e090b0d090068b361e6f3984ac9582c71dca0c
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-08 12:08:56 +0000

    updated token function

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,19 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
-    {
-        
+   try {
+    let token = req.header("Authorisation");
+
+    if (!token) {
+        return res.status(403).send("access Denied");
     }
-}
+
+    if (token.startsWith("Bearer")) {
+        token = token.slice(7, token.length).trimLeft();
+    }
+
+    } catch (err) {
+        res.status (500).json ({ error: err.message })
+    }
+};
+
