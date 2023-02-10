36eb1140bfe8d66a9f8d309fe7f42a8ebc245514
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-10 22:59:22 +0000

    edit verifyToken route

--- backend/index.js
+++ backend/index.js
@@ -57,7 +57,7 @@ const upload = multer({ storage });
 //it is middleware that is in between and run before it hit the end point login/register (logic that save the user into DB)
 //also we are going to create a register controller.
 app.post("/auth/register", upload.single("picture", register));
-app.post("/posts", verifyToken)
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes)
