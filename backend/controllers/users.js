f435d1ba8cc5d08eace552a207c278ef8290570e
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-10 22:40:23 +0000

    add formattedFriends

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -52,6 +52,12 @@ export const addRemoveFriend = async (req, res) => {
             user.friends.map((id) => User.findById(id))
         );
        
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath}) => {
+                return { _id, firstName, lastName, occupation, location, picturePath};
+            }
+        );
+        res.status(200).json(formattedFriends);
     } catch (err) {
         res.status(404).json({ message: err.message })
     }
