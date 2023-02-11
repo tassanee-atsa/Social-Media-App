bed7631e5f9d1c5aa08a43b1b72cda25132a615b
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-11 20:51:36 +0000

    added comment timestamps

--- backend/models/User.js
+++ backend/models/User.js
@@ -41,7 +41,7 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true },//to give us automatic timestamps to when it was created or updated.
   //Mongoose will add two properties of type date : createdAt and updatedAt (whenever you update the document
   //using save(), updateOne(), updateMany(), findOneAndUpdate(), update(), replaceOne(), or bulkWrite())
-);
+); // or it can be userSchema.set('timestamps', true)
 
 const User = mongoose.model("User", UserSchema);
 export default User;
