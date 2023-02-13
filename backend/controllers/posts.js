fe7468c696812275ca357a051aba6d5e3a80710f
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-13 23:56:23 +0000

    updated createPost post.js

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -2,5 +2,10 @@ import Post from "../models/Post.js"
 
 /* CREATE */
 export const createPost = async (req, res) => {
+    try{
+
+    } catch (err){
+        res.status(409).json({ message: err.message })
+    }
 
 };
