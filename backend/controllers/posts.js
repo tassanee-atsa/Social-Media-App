97c9c010e4dd40af4a1064c6599f7a1ea03485b7
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-13 23:58:37 +0000

    edited post.js controllers

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -3,6 +3,11 @@ import Post from "../models/Post.js"
 /* CREATE */
 export const createPost = async (req, res) => {
     try{
+        const { userId, description, picturePath } = req.body;
+        const user = await user.findById(userId);
+        const newPost = new Post ({
+            
+        })
 
     } catch (err){
         res.status(409).json({ message: err.message })
