78aae20a11cc0365c8574584e60eeaec4955838f
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-14 15:40:25 +0000

    added getUserPosts

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -19,9 +19,31 @@ export const createPost = async (req, res) => {
         await newPost.save();
 
         const post = await Post.find();
+        res.status(201).json(post); //Grab all the posts.
 
     } catch (err){
         res.status(409).json({ message: err.message })
     }
+};
 
+/* READ */
+export const getFeedPosts = async (req, res) => {
+    try {
+        const post = await Post.find();
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+
+    }
+};
+
+export const getUserPosts = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const post = await Post.find( { userId });
+        res.status(200).json(post);
+
+    } catch (err) {
+        res.status(404).json( {message: err.message })
+    }
 };
