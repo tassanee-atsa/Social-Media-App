e2aa957a0c54b0d7c58d2b1a18d2e3d7c11e8aaf
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-14 15:20:11 +0000

    update newPost function

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -7,11 +7,18 @@ export const createPost = async (req, res) => {
         const user = await user.findById(userId);
         const newPost = new Post ({
             userId,
-            fistName ,
-            lastName,
-            location,
-            description
+            fistName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            likes: {},
+            comments: []
         })
+        await newPost.save();
+
+        const post = await Post.find();
 
     } catch (err){
         res.status(409).json({ message: err.message })
