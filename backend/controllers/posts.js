acb26eb421248e9c72df3bf219606635782bdf25
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-14 22:28:51 +0000

    added likePost

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -47,3 +47,25 @@ export const getUserPosts = async (req, res) => {
         res.status(404).json( {message: err.message })
     }
 };
+
+/* UPDATE */
+export const likePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if (isLiked) {
+            post.likes.delete(userId)
+        } else {
+            post.likes.set(userId, true);
+        };
+
+        const updatedPost = await Post.findByIdAndUpdate(id, { likes: post.likes}, { new: true})
+
+        res.status(200).json(updatedPost);
+    } catch {
+        res.status(404).json({ message: err.message });
+    };
+};
