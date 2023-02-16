478b9feb3c8c12e6fa2c8b7942d607a4a19c8541
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-16 18:02:36 +0000

    import scenes page to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
-import HomePage from 'scenes/homePage' //In jsconfig.js, we have set up the term, so that dont need to do ../
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage'; //In jsconfig.js, we have set up the term, so that dont need to do ../
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+
 
 function App() {
+
+  
   return (
     <div className="app">
       
