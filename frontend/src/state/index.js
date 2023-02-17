eec6743e4338e2e1ce4752c6f3f056ea1cb8ae38
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-17 22:04:50 +0000

    export authSlice

--- frontend/src/state/index.js
+++ frontend/src/state/index.js
@@ -47,3 +47,8 @@ export const authSlice = createSlice({
     },
   },
 })
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+authSlice.actions;
+
+export default authSlice.reducer;
