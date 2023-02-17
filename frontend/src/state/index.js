010a0b521edf771a2e852659dfc0ab3d4c806e58
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-17 22:02:31 +0000

    added updatePosts

--- frontend/src/state/index.js
+++ frontend/src/state/index.js
@@ -1,41 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
 
-//This is the state that we will store in global state. so this can be used anywhere in the application, 
+//This is the state that we will store in global state. so this can be used anywhere in the application,
 //don't have to pass the state and properties.
 const initialState = {
-    mode: 'light',
-    user: null,
-    token: null,
-    posts: [],
+  mode: 'light',
+  user: null,
+  token: null,
+  posts: [],
 }
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        setMode: (state) => {
-            state.mode = state.mode === 'light'?  'dark' : 'light'
-        }, 
-        setLogin: (state, action) => {
-            state.user = action.payload.user;
-            state.token = action.payload.token;
-        },
+  name: 'auth',
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === 'light' ? 'dark' : 'light'
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user
+      state.token = action.payload.token
+    },
 
-        setLogout: (state) => {
-            state.user = null;
-            state.token = null;
-        },
+    setLogout: (state) => {
+      state.user = null
+      state.token = null
+    },
 
-        setFriends: (state, action) => {
-            if (state.user) {
-                state.user.friends = action.payload.friends;
-            } else {
-                console.error('user friends non-existent')
-            }
-        },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends
+      } else {
+        console.error('user friends non-existent')
+      }
+    },
 
-        setPosts: (state, action) => {
-            state.posts = action.payload.posts;
-        },
-    }
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts
+    },
+
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post_id) return action.payload.post;
+        return post
+      })
+      state.posts = updatedPosts;
+    },
+  },
 })
