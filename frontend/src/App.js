10561704a13ab74cc26871ccd468ee99645461e1
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-18 13:09:12 +0000

    import files to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from 'scenes/homePage'; //In jsconfig.js, we have set up the term, so that dont need to do ../
 import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
-
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/meterial/styles';
 
 function App() {
 
