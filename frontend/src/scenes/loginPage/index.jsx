0fbdcaa407bf06656b5363590b3b4e9377639ea7
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-19 21:36:04 +0000

    added loginPage function

--- frontend/src/scenes/loginPage/index.jsx
+++ frontend/src/scenes/loginPage/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material'
+
 const LoginPage = () => {
-    return (<div>loginpage</div>)
+  const theme = useTheme
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)')
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeigh="bold" fontSize="32px" color="primary">
+          Friendpage
+        </Typography>
+      </Box>
+    </Box>
+  )
 }
 
 export default LoginPage
