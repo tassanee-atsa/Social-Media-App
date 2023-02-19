71146294662461a2aa8bd67868a6249dc4ae4bfe
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-19 22:17:15 +0000

    set up loginSchema and Initialvalues

--- frontend/src/scenes/loginPage/Form.jsx
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -16,6 +16,7 @@ import { setLogin } from 'state';
 import Dropzone from 'react-dropzone'; // For dropping the files, let the user put the image, to upload the files.
 import Flexbetween from 'components/FlexBetween';
 
+//Form validation
 const registerSchema = yup.object().shape({
     firstName: yup.string().required('required'),
     lastName: yup.string().required('required'),
@@ -24,4 +25,26 @@ const registerSchema = yup.object().shape({
     location: yup.string().required('required'),
     occupation: yup.string().required('required'),
     picture: yup.string().required('required')
-})
+});
+
+const loginSchema = yup.object().shape({
+    email: yup.string().email('invalid email').required('required'),
+    password: yup.string().required('required'),
+});
+
+const initialValuesRegister = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    location: '',
+    occupation: '',
+    picture: '',
+}
+
+const initialValuesLogin = {
+    email: '',
+    password: '',
+};
+
+
