72ec5d100335b056831b5d0f8b0f9cee2aeeb4ae
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-20 13:58:51 +0000

    added register function Form.jsx

--- frontend/src/scenes/loginPage/Form.jsx
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -55,8 +55,35 @@ const Form = () => {
   const isNonMobile = useMediaQuery('(min-width: 600px)')
   const isLogin = pageType === 'login'
   const isRegister = pageType === 'register'
-  //
-  const handleFormSubmit = async (values, onSubmitProps) => {}
+
+  const register = async (values, onSubmitProps) => {
+    //This allows us to send form info with image
+    const formData = new FormData()
+    for (let value in values) {
+      //Loop through every key value in values object
+      formData.append(value, values[value])
+    }
+    formData.append('picturePath', values.picture.name)
+
+    const savedUserResponse = await fetch(
+      'http://localhost:3001/auth/register',
+      {
+        method: 'POST',
+        body: formData,
+      },
+    )
+    const savedUser = await savedUserResponse.json()
+    onSubmitProps.resetForm()
+
+    if (savedUser) {
+      setPageType('login')
+    }
+  }
+  
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    if (isLogin) await login(values, onSubmitProps)
+    if (isRegister) await register(values, onSubmitProps)
+  }
 
   return (
     <Formik
@@ -180,7 +207,7 @@ const Form = () => {
 
             <TextField
               label="Password"
-              type='password' //This type will hide a value
+              type="password" //This type will hide a value
               onBlur={handleBlur}
               onChange={handleChange}
               value={values.password}
@@ -189,6 +216,40 @@ const Form = () => {
               helperText={touched.password && errors.password}
               sx={{ gridColumn: 'span 4' }}
             />
+          </Box>
+
+          {/* BUTTONS */}
+          <Box>
+            <Button
+              fullWidth="submit"
+              sx={{
+                m: '2rem 0',
+                p: '1rem',
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                '&: hover': { color: palette.primary.main },
+              }}
+            >
+              {isLogin ? 'LOGIN' : 'REGISTER'}
+            </Button>
+            <Typography
+              onClick={() => {
+                setPageType(isLogin ? 'register' : 'login')
+                resetForm() // Clear the input field
+              }}
+              sx={{
+                textDecoration: 'underline',
+                color: palette.primary.main,
+                '& : hover': {
+                  cusor: 'pointer',
+                  color: palette.primary.light,
+                },
+              }}
+            >
+              {isLogin
+                ? "Don't have an account? Sign Up here."
+                : 'Already have an account? Login here'}
+            </Typography>
           </Box>
         </form>
       )}
