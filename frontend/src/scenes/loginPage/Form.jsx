ae13c8174fbac52d1dfe8ef7d164d6d3a1c31a3f
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-20 11:22:45 +0000

    added Dropzone picture file

--- frontend/src/scenes/loginPage/Form.jsx
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -14,7 +14,7 @@ import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux' //To store user information
 import { setLogin } from 'state'
 import Dropzone from 'react-dropzone' // For dropping the files, let the user put the image, to upload the files.
-import Flexbetween from 'components/FlexBetween'
+import FlexBetween from 'components/FlexBetween'
 
 //Form validation
 const registerSchema = yup.object().shape({
@@ -88,46 +88,82 @@ const Form = () => {
             {isRegister && (
               <>
                 <TextField
-                  label='First Name'
+                  label="First Name"
                   onBlur={handleBlur}
                   onChange={handleChange}
                   value={values.firstName}
-                  name='firstName'
-                  error={ Boolean(touched.firstName) && Boolean(errors.firstName) }
+                  name="firstName"
+                  error={
+                    Boolean(touched.firstName) && Boolean(errors.firstName)
+                  }
                   helperText={touched.firstName && errors.firstName}
                   sx={{ gridColumn: 'span 2' }}
                 />
-                 <TextField
-                  label='Last Name'
+                <TextField
+                  label="Last Name"
                   onBlur={handleBlur}
                   onChange={handleChange}
                   value={values.lastName}
-                  name='lastName'
-                  error={ Boolean(touched.lastName) && Boolean(errors.lastName) }
+                  name="lastName"
+                  error={Boolean(touched.lastName) && Boolean(errors.lastName)}
                   helperText={touched.lastName && errors.lastName}
                   sx={{ gridColumn: 'span 2' }}
                 />
-                  <TextField
-                  label='Location'
+                <TextField
+                  label="Location"
                   onBlur={handleBlur}
                   onChange={handleChange}
                   value={values.location}
                   name="location"
-                  error={ Boolean(touched.location) && Boolean(errors.location) }
+                  error={Boolean(touched.location) && Boolean(errors.location)}
                   helperText={touched.location && errors.location}
                   sx={{ gridColumn: 'span 4' }}
                 />
-                     <TextField
+                <TextField
                   label="Occupation"
                   onBlur={handleBlur}
                   onChange={handleChange}
                   value={values.occupation}
                   name="occupation"
-                  error={ Boolean(touched.occupation) && Boolean(errors.occupation) }
+                  error={
+                    Boolean(touched.occupation) && Boolean(errors.occupation)
+                  }
                   helperText={touched.occupation && errors.occupation}
                   sx={{ gridColumn: 'span 4' }}
                 />
-                
+                <Box
+                  gridColumn="span 4"
+                  border={`1px solid ${palette.neutral.medium}`}
+                  borderRadius="5px"
+                  p="1rem"
+                >
+                  <Dropzone
+                    acceptedFiles=".jpg, .jpeg, .png"
+                    multiple={false}
+                    onDrop={(acceptedFiles) =>
+                      setFieldValue('picture', acceptedFiles[0])
+                    }
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        p="1rem"
+                        sx={{ '&:hover': { cusor: 'pointer' } }}
+                      >
+                        <input {...getInputProps()} />
+                        {!values.picture ? (
+                          <p>Add Picture Here</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{values.picture.name}</Typography>
+                            <EditOutlinedIcon />
+                          </FlexBetween>
+                        )}
+                      </Box>
+                    )}
+                  </Dropzone>
+                </Box>
               </>
             )}
           </Box>
