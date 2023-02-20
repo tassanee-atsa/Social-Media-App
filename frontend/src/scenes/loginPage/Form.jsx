3bd9dbbcb8f4c73c463ff26a58fa69b4c84e7648
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-20 11:35:07 +0000

    added textField email and pass

--- frontend/src/scenes/loginPage/Form.jsx
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -166,6 +166,29 @@ const Form = () => {
                 </Box>
               </>
             )}
+
+            <TextField
+              label="Email"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              name="email"
+              error={Boolean(touched.email) && Boolean(errors.email)}
+              helperText={touched.email && errors.email}
+              sx={{ gridColumn: 'span 4' }}
+            />
+
+            <TextField
+              label="Password"
+              type='password' //This type will hide a value
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={Boolean(touched.password) && Boolean(errors.password)}
+              helperText={touched.password && errors.password}
+              sx={{ gridColumn: 'span 4' }}
+            />
           </Box>
         </form>
       )}
