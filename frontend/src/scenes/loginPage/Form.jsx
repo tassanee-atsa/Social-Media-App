9af4a54e8f8ef6b3cfe66c52e4fbe666aa559a0f
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-20 10:56:42 +0000

    added text fields in Form

--- frontend/src/scenes/loginPage/Form.jsx
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -1,50 +1,140 @@
-import  { useState } from 'react';
+import { useState } from 'react'
 import {
-    Box,
-    Button,
-    TextField,
-    useMediaQuery,
-    Typography,
-    useTheme
+  Box,
+  Button,
+  TextField,
+  useMediaQuery,
+  Typography,
+  useTheme,
 } from '@mui/material'
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
-import { Formik } from 'formik'; // Form library
-import * as yup from 'yup'; // Form validation library
-import { useNavigate } from 'react-router-dom'; 
-import { useDispatch } from 'react-redux'; //To store user information
-import { setLogin } from 'state';
-import Dropzone from 'react-dropzone'; // For dropping the files, let the user put the image, to upload the files.
-import Flexbetween from 'components/FlexBetween';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
+import { Formik } from 'formik' // Form library
+import * as yup from 'yup' // Form validation library
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux' //To store user information
+import { setLogin } from 'state'
+import Dropzone from 'react-dropzone' // For dropping the files, let the user put the image, to upload the files.
+import Flexbetween from 'components/FlexBetween'
 
 //Form validation
 const registerSchema = yup.object().shape({
-    firstName: yup.string().required('required'),
-    lastName: yup.string().required('required'),
-    email: yup.string().email('invalid email').required('required'),
-    password: yup.string().required('required'),
-    location: yup.string().required('required'),
-    occupation: yup.string().required('required'),
-    picture: yup.string().required('required')
-});
+  firstName: yup.string().required('required'),
+  lastName: yup.string().required('required'),
+  email: yup.string().email('invalid email').required('required'),
+  password: yup.string().required('required'),
+  location: yup.string().required('required'),
+  occupation: yup.string().required('required'),
+  picture: yup.string().required('required'),
+})
 
 const loginSchema = yup.object().shape({
-    email: yup.string().email('invalid email').required('required'),
-    password: yup.string().required('required'),
-});
+  email: yup.string().email('invalid email').required('required'),
+  password: yup.string().required('required'),
+})
 
 const initialValuesRegister = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    location: '',
-    occupation: '',
-    picture: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  location: '',
+  occupation: '',
+  picture: '',
 }
 
 const initialValuesLogin = {
-    email: '',
-    password: '',
-};
+  email: '',
+  password: '',
+}
 
+const Form = () => {
+  const [pageType, setPageType] = useState('login')
+  const { palette } = useTheme
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const isNonMobile = useMediaQuery('(min-width: 600px)')
+  const isLogin = pageType === 'login'
+  const isRegister = pageType === 'register'
+  //
+  const handleFormSubmit = async (values, onSubmitProps) => {}
 
+  return (
+    <Formik
+      onSubmit={handleFormSubmit}
+      initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
+      validationSchema={isLogin ? loginSchema : registerSchema}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+        resetForm,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <Box
+            display="grid"
+            gap="30px"
+            // Split the grid into 4 sections,
+            //minimum 0, otherwise will spilt into equal fraction of 4
+            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+            //Target the div of above key values as a child component
+            sx={{
+              '& > div': { gridColumn: isNonMobile ? undefined : 'span 4' }, //span 4 will take the whole grid of 4
+            }}
+          >
+            {isRegister && (
+              <>
+                <TextField
+                  label='First Name'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  name='firstName'
+                  error={ Boolean(touched.firstName) && Boolean(errors.firstName) }
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{ gridColumn: 'span 2' }}
+                />
+                 <TextField
+                  label='Last Name'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  name='lastName'
+                  error={ Boolean(touched.lastName) && Boolean(errors.lastName) }
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{ gridColumn: 'span 2' }}
+                />
+                  <TextField
+                  label='Location'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.location}
+                  name="location"
+                  error={ Boolean(touched.location) && Boolean(errors.location) }
+                  helperText={touched.location && errors.location}
+                  sx={{ gridColumn: 'span 4' }}
+                />
+                     <TextField
+                  label="Occupation"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.occupation}
+                  name="occupation"
+                  error={ Boolean(touched.occupation) && Boolean(errors.occupation) }
+                  helperText={touched.occupation && errors.occupation}
+                  sx={{ gridColumn: 'span 4' }}
+                />
+                
+              </>
+            )}
+          </Box>
+        </form>
+      )}
+    </Formik>
+  )
+}
+
+export default Form
