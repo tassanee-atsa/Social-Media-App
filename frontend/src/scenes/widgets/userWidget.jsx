5675fd7615ff9ead69d64b1979c67b7e162910ef
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-25 15:25:10 +0000

    add getUser function in userWidget.js

--- frontend/src/scenes/widgets/userWidget.jsx
+++ frontend/src/scenes/widgets/userWidget.jsx
@@ -4,7 +4,7 @@ import {
     LocationOnOutlined,
     WorkOutlineOutlined,
 } from '@mui/icons-material';
-import { Box, Typography, Divider, UseTheme } from '@mui/material';
+import { Box, Typography, Divider, useTheme } from '@mui/material';
 import UserImage from '../../components/UserImage';
 import FlexBetween from '../../components/FlexBetween';
 import WidgetWrapper from '../../components/WidgetWrapper';
@@ -12,3 +12,46 @@ import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const UserWidget = ({ userId, picturePath }) => {
+    const [user, setUser] = useState(null);
+    const { palette } = useTheme();
+    const navigate = useNavigate();
+    const token = useSelector((state) => state.token);
+    const dark = palette.neutral.dark;
+    const medium = palette.neutral.medium;
+    const main = palette.neutral.main;
+
+    const getUser = async () => {
+        const response = await fetch(`http://localhost:3001/users/${userId}`, 
+        {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}`},
+        });
+        const data = await response.json();
+        setUser(data);
+    };
+
+    useEffect(() => {
+        getUser();
+    }, []) //eslint-disable-line react-hooks/exaustive-deps...When we enter the page, getUser will be called when this component is render for the first time.
+    
+    //o prevent page error. If there is no user, don't return anything yet.
+    if (!user) {
+        return null;
+    }
+
+    const {
+        firstName,
+        lastName,
+        location,
+        occupation,
+        viewedProfile,
+        impressions,
+        friends,
+    } = user;
+
+    return (
+        
+    )
+
+};
