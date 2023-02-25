30bff52c6941c5ad0d8723eae9b4d8719e363250
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-25 16:51:49 +0000

    create Homepage Function

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import Navbar from '../navbar';
-
+import React from 'react'
+import { Box } from '@mui/material'
+import Navbar from '../navbar'
 
 const HomePage = () => {
-    return (<Box>
-        <Navbar />
-    </Box>)
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)')
+  const { _id, picturePath } = userSelector((state) => state.user)
+
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? 'flex' : 'block '}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        
+      </Box>
+    </Box>
+  )
 }
 
 export default HomePage
