d92b7428048bf4e1a6bde732ec493309f4b7258a
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-25 15:03:48 +0000

    add userWidget

---
+++ frontend/src/scenes/widgets/userWidget.jsx
@@ -0,0 +1,14 @@
+import {
+    ManageAccoutsOutlined,
+    EditOutlined,
+    LocationOnOutlined,
+    WorkOutlineOutlined,
+} from '@mui/icons-material';
+import { Box, Typography, Divider, UseTheme } from '@mui/material';
+import UserImage from '../../components/UserImage';
+import FlexBetween from '../../components/FlexBetween';
+import WidgetWrapper from '../../components/WidgetWrapper';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
