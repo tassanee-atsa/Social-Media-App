8fadb4bfb1be9111dc7c888239c581c95b62668e
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-27 21:34:59 +0000

    update PostsWidget

--- frontend/src/scenes/widgets/PostsWidget.jsx
+++ frontend/src/scenes/widgets/PostsWidget.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setPosts } from '../../state';
-import PostWidget from './PostWidget';
+import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setPosts } from '../../state'
+import PostWidget from './PostWidget'
 
+const PostsWidget = (userId, isProfile = false) => {
+  const dispatch = useDispatch()
+  const posts = useSelector((state) => state.posts)
+  const token = useSelector((state) => state.token)
+
+  const getPosts = async () => {
+
+    const response = await fetch('http://localhost:3001/posts', {
+        methods: 'GET',
+        header: { Authorisation: `Bearer ${token}`}
+    })
+
+    const data = response.json();
+    
+  }
+}
+
+export default PostsWidget
