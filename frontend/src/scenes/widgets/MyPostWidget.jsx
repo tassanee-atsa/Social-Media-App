b1f39dbcce5ad874c118e40939a7df89e0646c01
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-27 13:02:01 +0000

    updated MyPostWidget function

--- frontend/src/scenes/widgets/MyPostWidget.jsx
+++ frontend/src/scenes/widgets/MyPostWidget.jsx
@@ -19,23 +19,114 @@ import {
   IconButton,
   useMediaQuery,
 } from '@mui/icons-material'
-import FlexBetween from '../../components/FlexBetween';
-import Dropzone from 'react-dropzone';
-import UserImage from '../../components/UserImage';
-import WidgetWrapper from '../../components/WidgetWrapper';
-import { useState } from 'react';
-import { useDispatch , useSelector } from 'react-redux';
-import { serPosts } from 'state';
+import FlexBetween from '../../components/FlexBetween'
+import Dropzone from 'react-dropzone'
+import UserImage from '../../components/UserImage'
+import WidgetWrapper from '../../components/WidgetWrapper'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setPosts } from 'state'
 
 const MyPostWidget = ({ picturePath }) => {
-    const dispatch = useDispatch();
-    const [isImage, setIsImage] = useState(false);
-    const [image, setImage] = useState(null);
-    const [post, setPost] = useState('');
-    const { palellte } = useTheme();
-    const { _id } = useSelector((state)=> state.user);
-    const token = useSelector((state) => state.token);
-    
+  const dispatch = useDispatch()
+  const [isImage, setIsImage] = useState(false) //switch the state when a user clicks on an option to drop an image.
+  const [image, setImage] = useState(null) // actual image when user drop an image.
+  const [post, setPost] = useState('') //The actual post content
+  const { palellte } = useTheme()
+  const { _id } = useSelector((state) => state.user)
+  const token = useSelector((state) => state.token)
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)')
+  const mediumMain = palette.neutral.mediumMain
+  const medium = palette.neutral.medium
 
+  const handlePost = async () => {
+    const formData = new FormData()
+    FormData.append('userId', _id)
+    FormData.append('description', post)
+    if (image) {
+      formData.append('picture', image)
+      formData.append('picturePath', image.name)
+    }
+    const response = await fetch(`http:localhost:3001/posts`, {
+      methed: 'POST',
+      headers: { Authorisation: `Bearer ${token}` },
+      body: formData,
+    })
+    const posts = await response.json()
+    dispatch(setPosts({ posts })) //To keep the list of posts
+    setImage(null) //reset all the state after the api is called.
+    setPost('')
+  }
 
+  return (
+    <WidgetWrapper>
+      <FlexBetween gap="1.5rem">
+        <UserImage image={picturePath} />
+        <InputBase placeholder="What is in your mind..." />
+        onChange={(e) => setPost(e.target.value)}
+        value={post}
+        sx=
+        {{
+          width: '100%',
+          backgroundColor: palette.neutral.light,
+          borderRadius: '2rem',
+          padding: '1rem 2 rem',
+        }}
+      </FlexBetween>
+      {isImage && (
+        <Box
+          border={`1px solid ${medium}`}
+          borderRadius="5px"
+          mt="1rem"
+          p="1rem"
+        >
+          <Dropzone
+            acceptedFiles=".jpg, .jpeg, .png"
+            multiple={false}
+            onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <FlexBetween>
+                <Box
+                  {...getRootProps()}
+                  border={`2px dashed ${palette.primary.main}`}
+                  p="1rem"
+                  width="100%"
+                  sx={{ '&:hover': { cusor: 'pointer' } }}
+                >
+                  <input {...getInputProps()} />
+                  {!image ? (
+                    <p>Add Image Here</p>
+                  ) : (
+                    <FlexBetween>
+                      <Typography>{image.name}</Typography>
+                      <EditOutlined />
+                    </FlexBetween>
+                  )}
+                </Box>
+                {image && (
+                  <IconButton
+                    onClick={() => setImage(null)}
+                    sx={{ width: '15%' }}
+                  >
+                    <DeleteOutlined />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            )}
+          </Dropzone>
+        </Box>
+      )}
+
+      <Divider sx={{ margin: '1.25rem 0'}} />
+
+      <FlexBetween>
+      {/* turn off and open image dropzone */}
+        <FlexBetween gap='0.25rem' onClick={()=> setIsImage(!isImage)}>
+        
+
+        </FlexBetween>
+      </FlexBetween>
+    </WidgetWrapper>
+  )
 }
