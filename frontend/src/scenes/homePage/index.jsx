682dfee4bdd584b151794d9cf223ae0961bd2d3b
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-28 21:21:41 +0000

    create FriendListWidget

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -6,6 +6,7 @@ import UserWidget from '../widgets/UserWidget'
 import MyPostWidget from '../widgets/MyPostWidget'
 import PostsWidget from '../widgets/PostsWidget'
 import AdvertWidget from '../widgets/AdvertWidget'
+import FriendListWidget from '../widgets/FriendListWidget'
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width: 1000px)')
@@ -34,6 +35,7 @@ const HomePage = () => {
           <Box flexBasis="26%">
             <AdvertWidget />
             <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
