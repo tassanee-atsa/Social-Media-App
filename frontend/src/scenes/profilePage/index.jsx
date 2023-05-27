6221cb0324d2d2caee40b7b42b9d1ba19d2fc64c
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-05-27 14:35:05 +0100

    more pathed imports

--- frontend/src/scenes/profilePage/index.jsx
+++ frontend/src/scenes/profilePage/index.jsx
@@ -4,10 +4,10 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Navbar from "../navbar";
-import FriendListWidget from "../widgets/FriendListWidget";
-import MyPostWidget from "../widgets/MyPostWidget";
-import PostsWidget from "../widgets/PostsWidget";
-import UserWidget from "../widgets/UserWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const ProfilePage = () => {
   const [user, setUser] = useState(null);
