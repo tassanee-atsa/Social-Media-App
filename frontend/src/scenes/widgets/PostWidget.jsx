c569e4aa1499ac2e40539f4925fed22bddc1af06
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-06-01 20:51:46 +0100

    delete unuse icons

--- frontend/src/scenes/widgets/PostWidget.jsx
+++ frontend/src/scenes/widgets/PostWidget.jsx
@@ -5,7 +5,7 @@ import {
   FavoriteOutlined,
   ShareOutlined,
 } from "@mui/icons-material";
-import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import { IconButton, Typography, useTheme } from "@mui/material";
 import FlexBetween from "../../components/FlexBetween";
 import Friend from "../../components/Friend";
 import WidgetWrapper from "../../components/WidgetWrapper";
